ba96ef9a7cc0014b8f198d5417cd88d73960bcbf
philips-of-ng@philips <philipsedun@gmail.com>
2026-07-16 22:01:09 +0100

    ok2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import CheckIn from './pages/CheckIn'
-import { Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // Aliased here
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/checkin' Component={<CheckIn />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/checkin' element={<CheckIn />} /> {/* Changed to element */}
+      </Routes>
+    </Router>
   )
 }
 
